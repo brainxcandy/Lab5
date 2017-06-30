45d5281eca2dbc1d69675d6aef7aa6c9f13763a1
brainxcandy <amber.hoey@yahoo.com>
2017-06-30 08:56:22 -0400

    initial commit

---
+++ fahrenheit.js
@@ -0,0 +1,10 @@
+function convertToF(celsius) {
+var fahrenheit;
+temp = 9/5;
+x = 32;
+var currenttemp
+fahrenheit = celsius * temp + 32;
+console.log(fahrenheit);
+return fahrenheit;
+}
+convertToF(90);
